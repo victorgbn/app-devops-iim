005a410a068c24b01bf80a9c075b30b81a11943b
Théo Popelin <t.popelin@reussir.fr>
2022-07-05 16:42:28 +0200

    ajout bouton front

--- src/views/Dashboard/Main.vue
+++ src/views/Dashboard/Main.vue
@@ -1,3 +1,26 @@
 <template>
-    <span>Hello Word</span>
+    <v-container>
+        <h1>Titre</h1>
+        <v-btn @click="createAnalyse">
+            Action
+        </v-btn>
+    </v-container>
 </template>
+
+<script>
+import { API } from '@vue/composition-api'
+
+ export default {
+     methods: {
+         async createAnalyse() {
+             try {
+                 console.log('action')
+                 await API.post('analyses', '/createAnalyse')
+             }
+             catch(e) {
+                 console.log(e)
+             }
+         }
+     }
+    }
+</script>
