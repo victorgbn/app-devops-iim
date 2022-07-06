3d0c77c0e8dbadd818726dea070cc0f2dd4a677b
victorgbn <victor.gaubin@gmail.com>
2022-07-06 11:59:21 +0200

    add user id  in storage

--- amplify/backend/function/createUser/src/index.py
+++ amplify/backend/function/createUser/src/index.py
@@ -1,15 +1,29 @@
 import json
+from datetime import datetime
+import os
+import boto3
+from botocore.exceptions import ClientError
 
 def handler(event, context):
   print('received event:')
   print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+
+  try:
+    if event['triggerSource'] != "PostConfirmation_ConfirmSignUp":
+      raise ValueError("Error event")
+    resource = boto3.resource('dynamodb')
+    table_name = os.environ['STORAGE_USERS_NAME']
+    table = resource.Table(table_name)
+    create_user(table, event)
+    return event
+  except (Exception, ClientError) as error:
+    print(error)
+
+def create_user(table, event):
+    currentDate = f'{str(datetime.now().isoformat())}Z'
+    user = {
+      'id' : event['request']['userAttributes']['sub'],
+      'updateDate': currentDate,
+      'analyses': []
+    }
+    table.put_item(Item=user)
