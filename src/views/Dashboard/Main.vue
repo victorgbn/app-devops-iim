0db8481958202cd4c7a1cb58b7563994e1f35459
Valentin <wdcvalentin@yahoo.fr>
2022-07-07 11:01:56 +0200

    feat(analysis): fetching users analysis

--- src/views/Dashboard/Main.vue
+++ src/views/Dashboard/Main.vue
@@ -1,26 +1,64 @@
 <template>
-    <v-container>
-        <h1>Titre</h1>
-        <v-btn @click="createAnalyses">
-            Action
-        </v-btn>
-    </v-container>
+  <v-container>
+    <h1>Titre</h1>
+    <v-btn @click="createAnalyses"> Action </v-btn>
+
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">ID</th>
+            <th class="text-left">title</th>
+            <th class="text-left">description</th>
+            <th class="text-left">transcript</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in usersAnalysis" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.description }}</td>
+            <td>{{ item.transcript }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
-
 <script>
-import { API } from 'aws-amplify'
+import { API } from "aws-amplify";
 
- export default {
-     methods: {
-         async createAnalyses() {
-             try {
-                 console.log('action')
-                 await API.post('analyses', '/createAnalyses')
-             }
-             catch(e) {
-                 console.log(e)
-             }
-         }
-     }
-    }
+export default {
+  data() {
+    return {
+      usersAnalysis: null,
+    };
+  },
+  methods: {
+    async createAnalyses() {
+      try {
+        console.log("action");
+        await API.post("analyses", "/createAnalyses");
+      } catch (e) {
+        console.log(e);
+      }
+    },
+
+    async getAnalysis() {
+      try {
+        const { analyseObjects } = await API.get(
+          "analyses",
+          "/getUserAnalysis"
+        );
+        console.log("analyseObjects", analyseObjects);
+        this.usersAnalysis = analyseObjects;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  mounted() {
+    this.getAnalysis();
+  },
+};
 </script>
